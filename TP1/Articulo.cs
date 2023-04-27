5ffaafb8cdc1725d1fcdf4e481bba9c625439332
Coqui2 <francomarcolongo98@gmail.com>
2023-04-27 10:26:58 -0300

    Base tp

---
+++ TP1/Articulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TP1
+{
+    internal class Articulo
+    {
+        private int codigo;
+        private string nombre;
+        private string descripcion;
+        Marca marca;
+        Categoria categoria;
+        List<string> imagenes;
+        private float precio;
+    }
+}
+
