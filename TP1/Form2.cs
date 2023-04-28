e2ff5cb396e3ed90a728831f0826758abce0e464
Coqui2 <francomarcolongo98@gmail.com>
2023-04-28 00:02:37 -0300

    v0.4

--- TP1/Form2.cs
+++ TP1/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,8 @@ namespace TP1
         {
 
             InitializeComponent();
+            this.Controls.Add(listView1);
+            listView1.View= View.Details;
             this.Load += Form2_Load;
         }
 
@@ -24,10 +27,36 @@ namespace TP1
             this.Dock = DockStyle.Fill;
             this.AutoSize = false;
             this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            listView1.Columns.Add("Nombre", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Marca", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Categoria", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Precio", -2, HorizontalAlignment.Left);
+
+            // BaseDeDatos db = new BaseDeDatos();
+            List<Articulo> articulos= new List<Articulo>();
+            //articulos = db.listarArticulos();
+            
+            Marca prueba = new Marca(1,"hola");
+            Categoria prueba2 = new Categoria(1,"chau");
+            Articulo art1 = new Articulo(1,"art1","desc1",prueba,prueba2,null,100);
+            Articulo art2 = new Articulo(2, "art2", "desc2", prueba, prueba2, null, 200);
+            Articulo art3 = new Articulo(3, "art3", "desc3", prueba, prueba2, null, 300);
+            articulos.Add(art1);
+            articulos.Add(art2);
+            articulos.Add(art3);
+
+            
+            foreach (Articulo articulo in articulos)
+            {
+                ListViewItem item = new ListViewItem(new[] {articulo.Nombre,articulo.Marca.Nombre,articulo.Categoria.Nombre,articulo.Precio.ToString()});
+                listView1.Items.Add(item);
+            }
+
+
         }
 
 
-        private void label1_Click(object sender, EventArgs e)
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
